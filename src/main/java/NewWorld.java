0fe62b401329a8917861406bc939f795a4b1951c
ujjwalk <ujjwalkeshri@hotmail.com>
2025-04-29 04:19:41 +0530

    Second Commit

--- src/main/java/NewWorld.java
+++ src/main/java/NewWorld.java
@@ -3,6 +3,9 @@ public class NewWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("New World");
+		System.out.println("New dreams");
 
 	}
 
